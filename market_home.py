980dc404d6fa178037c885cc827f3d7fa0f9266f
Aurélien Girin <aurelien.girin@gmail.com>
2023-01-31 09:57:06 +0100

    add comments

--- market_home.py
+++ market_home.py
@@ -5,6 +5,7 @@ import random
 import select
 import signal
 import socket
+import sys
 import sysv_ipc
 import time
 import tkinter as tk
@@ -15,7 +16,7 @@ from termcolor import colored
 
 PORT = 6666
 HOST = "localhost"
-worldWar, trumpElection, fuelShortage = 0, 0, 0
+worldWar, nucDisaster, fuelShortage = 0, 0, 0
 initStock = 15
 initTemp = 0
 initPrice = 0.1740
@@ -27,11 +28,11 @@ def home(s, tradePol, keyMsg, keyEng, prodRate, consRate):
     global PORT
     stockHome = s
     pid = os.getpid()
-    print(f'my pid is {pid} and my stock is {stockHome}')
+    print(f'My pid is {pid} and my stock is {stockHome}') #initial stock of each home
     mqMsg = sysv_ipc.MessageQueue(keyMsg)
     mqEng = sysv_ipc.MessageQueue(keyEng)
     while True:
-        stockHome=stockManager(prodRate, consRate, stockHome);
+        stockHome=stockManager(prodRate, consRate, stockHome)
         stockHome=tradePolicy(tradePol, stockHome, pid, mqMsg, mqEng)
         time.sleep(delay/1000)
 def stockManager(prodRate, consRate, stockHome):
@@ -40,24 +41,24 @@ def stockManager(prodRate, consRate, stockHome):
     return stockHome
 
 
-def tradePolicy(policy, stockHome, pid, mqMsg, mqEng):#1 don, 2 vente, 3 vente si personne a qui donner
+def tradePolicy(policy, stockHome, pid, mqMsg, mqEng):
     if stockHome > threshold:
         match policy:
-            case 1:
-                stockHome = donEnergie(stockHome, mqMsg, mqEng, pid)
-            case 2:
-                typeTransac = 2 #surplus
+            case 1: #always give away
+                stockHome = giveaway(stockHome, mqMsg, mqEng, pid)
+            case 2: #always sell
+                typeTransac = 2
                 stockHome=homeSelling(typeTransac, pid, stockHome)
-            case 3:
+            case 3: #give or sell
                 stockHome=debarras(stockHome, mqMsg, mqEng, pid)
-    elif stockHome < threshold :
+    elif stockHome < threshold:
         stockHome=homeRestock(stockHome, pid, mqMsg, mqEng)
     else:
         pass
     return stockHome
 
 
-def donEnergie(stockHome, mqMsg, mqEng, pid):
+def giveaway(stockHome, mqMsg, mqEng, pid):
     pidH=0
     try:
         m, pidH = mqMsg.receive(block=False)
@@ -130,7 +131,7 @@ def homeBuying(typeTransac, pid, stockHome):
 
 
 def external():
-    time.sleep(5*delay/1000)
+    time.sleep(5*delay/1000) #to avoid external event a the beginning of the simulation
     while True:
         r = random.randint(0, 20)
         match r:
@@ -147,7 +148,7 @@ def external():
                 pass
         time.sleep(delay/1000)
 def handler(sig, frame):
-    global worldWar, trumpElection, fuelShortage
+    global worldWar, nucDisaster, fuelShortage
     if sig == signal.SIGUSR1:
         if worldWar == 0:
             print("--------------------------------------------------")
@@ -160,16 +161,16 @@ def handler(sig, frame):
             print("--------------------------------------------------")
             worldWar = 0
     elif sig == signal.SIGUSR2:
-        if trumpElection == 0:
+        if nucDisaster == 0:
             print("--------------------------------------------------")
-            print(colored("Start of Trump election", "red"))
+            print(colored("Start of nuclear disaster", "red"))
             print("--------------------------------------------------")
-            trumpElection = 1
+            nucDisaster = 1
         else:
             print("--------------------------------------------------")
-            print(colored("End of Trump election", "green"))
+            print(colored("End of nuclear disaster", "green"))
             print("--------------------------------------------------")
-            trumpElection = 0
+            nucDisaster = 0
     elif sig == signal.SIGALRM:
         if fuelShortage == 0:
             print("--------------------------------------------------")
@@ -181,16 +182,13 @@ def handler(sig, frame):
             print(colored("End of fuel shortage", "green"))
             print("--------------------------------------------------")
             fuelShortage = 0
-
-def handlerC(sig, frame):
-    print('CTRL+C was pressed. Exit program')
-    mqEng.remove()
-    mqMsg.remove()
-    displayMarket.destroy()
-    displayTemp.destroy()
-    os.exit()
-
-
+    elif sig == signal.SIGINT:
+        print('CTRL+C was pressed. Exit program')
+        mqEng.remove()
+        mqMsg.remove()
+        displayMarket.destroy()
+        displayTemp.destroy()
+        sys.exit()
 
 
 def weather(temp):
@@ -205,32 +203,31 @@ def weather(temp):
 
 def priceCalcul(price, temp):
     mutex = Lock()
-    global worldWar, trumpElection, fuelShortage
-    a = 0.00003
-    b1 = 0.05
-    b2 = 0.005
-    b3 = 0.005
-    g = 0.99
+    global worldWar, nucDisaster, fuelShortage
+    a = 0.00003 #coef for contribution of temp
+    b1 = 0.02 #coef for contribution of world war
+    b2 = 0.005 #coef for contribution of nuclear disaster
+    b3 = 0.005 #coef for contribution of fuel shortage
+    g = 0.99 #attenuation coef
     t = temp.value
     p = price.value
     mutex.acquire()
-    price.value = g * p + a * (t * t - 40 * t + 375) #+ b1 * worldWar + b2 * trumpElection + b3 * fuelShortage
+    price.value = g * p + a * (t * t - 40 * t + 375) + b1 * worldWar + b2 * nucDisaster + b3 * fuelShortage #calcul of energy price according to temp and events
     mutex.release()
 
 
 def market(temp, price, stock):
-
     externalProcess = Process(target=external)
     externalProcess.start()
     signal.signal(signal.SIGUSR1, handler)
     signal.signal(signal.SIGUSR2, handler)
     signal.signal(signal.SIGALRM, handler)
     global PORT
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as market_socket:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as market_socket: #setup of socket conn
         market_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         market_socket.bind((HOST, PORT))
-        market_socket.listen(8)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        market_socket.listen(5) #setting the max numver of simultaneous conn
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: #threadpool for the handling of transactions with homes
             while True:
                 priceCalcul(price, temp)
                 readable, writable, error = select.select([market_socket], [], [], 1)
@@ -270,15 +267,17 @@ def MarketTransaction(home_socket, address, stock):
 
 if __name__ == "__main__":
 
-    signal.signal(signal.SIGINT, handlerC)
+    signal.signal(signal.SIGINT, handler) #for the end of the simulation
 
     sharedTemp = Value('f', initTemp)
     sharedPrice = Value('f', initPrice)
     sharedStock = Value('I', initStock)
+
     temp = initTemp
     price = initPrice
     stock = initStock
 
+    #temperature display
     displayTemp = tk.Tk()
     displayTemp.title("Temperature")
     displayTemp.geometry('300x200+660+320')
@@ -286,6 +285,7 @@ if __name__ == "__main__":
     labelTemp.config(font=('verdana', 12))
     labelTemp.pack()
 
+    #price and market stock display
     displayMarket = tk.Tk()
     displayMarket.title("Market")
     displayMarket.geometry('300x200+960+320')
@@ -293,26 +293,30 @@ if __name__ == "__main__":
     labelMarket.config(font=('verdana', 12))
     labelMarket.pack()
 
-    keyMsg = 105
-    keyEng = 205
+
+    #setup of mq
+    keyMsg = 100
+    keyEng = 200
     mqMsg = sysv_ipc.MessageQueue(keyMsg, sysv_ipc.IPC_CREX)
     mqEng = sysv_ipc.MessageQueue(keyEng, sysv_ipc.IPC_CREX)
 
-    home1 = Process(target=home, args=(35, 1, keyMsg, keyEng, 1, 2))
-    home2 = Process(target=home, args=(28, 2, keyMsg, keyEng, 1, 1))
-    home3 = Process(target=home, args=(32, 3, keyMsg, keyEng, 2, 1))
 
+    #setup of our processes
+    home1 = Process(target=home, args=(35, 1, keyMsg, keyEng, 1, 2,))
+    home2 = Process(target=home, args=(28, 2, keyMsg, keyEng, 1, 1,))
+    home3 = Process(target=home, args=(32, 3, keyMsg, keyEng, 2, 1,))
     weatherProcess = Process(target=weather, args=(sharedTemp,))
     marketProcess = Process(target=market, args=(sharedTemp, sharedPrice, sharedStock,))
 
     weatherProcess.start()
     marketProcess.start()
-    time.sleep(0.2)
+    time.sleep(0.2) #to avoid homes trying to connect market before market is ready to accept connection
 
     home1.start()
     home2.start()
     home3.start()
 
+    #update of displays
     def update_temp():
         temp = sharedTemp.value
         labelTemp.config(text=f'\n\nCurrent temperature : \n\n{temp:.1f} °C')
@@ -328,7 +332,6 @@ if __name__ == "__main__":
         price = sharedPrice.value
         labelMarket.config(text=f'\nCurrent price : \n\n{price:.4f} €/kWh\n\n Market stock : {stock} kWh')
         displayMarket.after(delay, update_price)
-
     update_temp()
     update_price()
     displayTemp.mainloop()
